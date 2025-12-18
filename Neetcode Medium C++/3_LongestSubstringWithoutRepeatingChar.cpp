abfe9ea58aaa49cdeb1cae6b04b1d0de227562d2
AbuObaida01 <obaidaabu5200@gmail.com>
2025-12-18 23:51:41 +0530

    Day 1

---
+++ Neetcode Medium C++/3_LongestSubstringWithoutRepeatingChar.cpp
@@ -0,0 +1,21 @@
+#include<iostream> 
+#include<unordered_set>
+using namespace std;
+
+int LengthofLong(string s){
+    unordered_set<char> st;
+    int l=0,ans=0;
+    for(int r=0;r<s.size();r++){
+        while(st.count(s[r])){
+            st.erase(s[l]);
+            l++;
+        }
+        st.insert(s[r]);
+        ans=max(ans,r-l+1);
+    }
+    return ans;
+}
+int main(){
+    string s="ababbb";
+    cout<<LengthofLong(s);
+}
